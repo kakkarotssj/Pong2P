09457dd9191b801b5705f2aabf468d59f20f85c7
Sid <kanjeckcooldeva.005@gmail.com>
2016-08-20 00:59:07 +0530

    factors changed

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty,ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
-from jnius import autoclass
-
 
 class PongPaddle(Widget):
     score = NumericProperty(0)
@@ -13,8 +11,8 @@ class PongPaddle(Widget):
         if self.collide_widget(ball):
             vx, vy = ball.velocity
             bounce_factor = (ball.center_y - self.center_y) / (self.height / 2)
-            bounced = Vector(-1.1 * vx, vy)
-            vel = bounced * 1.1
+            bounced_back = Vector(-1.1 * vx, vy)
+            vel = bounced_back * 1.1
             ball.velocity = vel.x, vel.y + bounce_factor
 
 
@@ -25,8 +23,8 @@ class PongPaddles(Widget):
         if self.collide_widget(ball):
             vx,vy=ball.velocity
             bounce_factor=(ball.center_x-self.center_x)/(self.height/2)
-            bounced=Vector(-1*vy,vx)
-            vel=bounced*1.01
+            bounced_back=Vector(-1.1*vy,vx)
+            vel=bounced_back*1.1
             ball.velocity=vel.y,vel.x+bounce_factor
 
 
@@ -47,57 +45,54 @@ class PongGame(Widget):
     player4 = ObjectProperty(None)
 
 
-    def serve_ball(self, vel=(20, 0)):
+    def ball_again(self, vel=(20, 0)):
         self.ball.center = self.center
         self.ball.velocity = vel
 
-    def update(self, dt):
+    def update_function(self, dt):
         self.ball.move()
 
-        #bounce of paddles
-        self.player1.bounce_ball(self.ball)
-        self.player2.bounce_ball(self.ball)
-        self.player3.bounce_ball(self.ball)
-        self.player4.bounce_ball(self.ball)
+        self.player_l.bounce_ball(self.ball)
+        self.player_r.bounce_ball(self.ball)
+        self.player_u.bounce_ball(self.ball)
+        self.player_d.bounce_ball(self.ball)
 
-        #bounce ball off bottom or top
         if (self.ball.y < self.y) or (self.ball.top > self.top):
             self.ball.velocity_y *= -1.05
         if (self.ball.x<self.x)or(self.ball.top>self.top):
             self.ball.velocity_x*=-1.05
 
-        #went of to a side to score point?
         if self.ball.x < self.x:
-            self.player2.score += 1
-            self.serve_ball(vel=(20, 0))
+            self.player_r.score += 1
+            self.ball_again(vel=(20, 0))
         if self.ball.x > self.width:
-            self.player1.score += 1
-            self.serve_ball(vel=(-20, 0))
+            self.player_l.score += 1
+            self.ball_again(vel=(-20, 0))
         if self.ball.y > self.height:
-            self.player4.score += 1
-            self.serve_ball(vel=(0,-20))
+            self.player_d.score += 1
+            self.ball_again(vel=(0,-20))
         if self.ball.y < self.y:
-            self.player3.score += 1
-            self.serve_ball(vel=(0,20))
+            self.player_u.score += 1
+            self.ball_again(vel=(0,20))
 
     def on_touch_move(self, touch):
-        if (touch.x<self.player1.center_x+149)and(touch.x>self.player1.center_x-149):
-            self.player1.center_y = touch.y
-        if (touch.x<self.player2.center_x+149)and(touch.x>self.player2.center_x-149):
-            self.player2.center_y = touch.y
-        if (touch.y<self.player3.center_y+266)and(touch.y>self.player3.center_y-266):
-            self.player3.center_x = touch.x
-        if (touch.y<self.player4.center_y+266)and(touch.y>self.player4.center_y-266):
-            self.player4.center_x = touch.x
+        if (touch.x<self.player_l.center_x+149)and(touch.x>self.player_l.center_x-149):
+            self.player_l.center_y = touch.y
+        if (touch.x<self.player_r.center_x+149)and(touch.x>self.player_r.center_x-149):
+            self.player_r.center_y = touch.y
+        if (touch.y<self.player_u.center_y+266)and(touch.y>self.player_u.center_y-266):
+            self.player_u.center_x = touch.x
+        if (touch.y<self.player_d.center_y+266)and(touch.y>self.player_d.center_y-266):
+            self.player_d.center_x = touch.x
 
 
 class PongApp(App):
     def build(self):
-        game = PongGame()
-        game.serve_ball()
-        Clock.schedule_interval(game.update, 1.0 / 100.0)
-        return game
+        game_pong = PongGame()
+        game_pong.ball_again()
+        Clock.schedule_interval(game_pong.update_function, 1.0 / 100.0)
+        return game_pong
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     PongApp().run()
